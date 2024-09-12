bb0cf4c917b5b5f3e115e850253a31d44f240e4c
Kimmm-c <97310663+Kimmm-c@users.noreply.github.com>
2024-09-12 12:27:18 -0700

    implemented Map helper functions

--- src/Map/Map.cpp
+++ src/Map/Map.cpp
@@ -4,7 +4,7 @@ Map::Map(uint8_t w, uint8_t h)
         : width(w), height(h) {
     // Construct a graph with disconnected vertices
     for (uint8_t y; y < height; y++) {
-        for (uint8_t x; x < width; x++ ) {
+        for (uint8_t x; x < width; x++) {
             // emplace allows constructing a pair and a vector directly inside the map
             // without creating temporary objects.
             graph.emplace(make_pair(x, y), vector<pair<uint8_t, uint8_t>>{});
@@ -12,16 +12,22 @@ Map::Map(uint8_t w, uint8_t h)
     }
 
     // Construct the winning path
+    vector<bool> visit = {true, false};
     pair<uint8_t, uint8_t> currentVertex = {0, 0};
 
-    winningPath.push_back(currentVertex);
+    winningPath.insert(currentVertex);
 
     while (currentVertex.first != width - 1 && currentVertex.second != height - 1) {
-        vector<pair<uint8_t, uint8_t>> neighbors = {{currentVertex.first - 1, currentVertex.second},
-                                                    {currentVertex.first + 1, currentVertex.second},
-                                                    {currentVertex.first, currentVertex.second - 1},
-                                                    {currentVertex.first, currentVertex.second + 1}};
+        vector<pair<uint8_t, uint8_t>> neighbors = getNeighbours(currentVertex);
 
+        // Filter out invalid neighbors
+        filterByCoordinate(neighbors);
+        filterByVisit(neighbors);
+
+        uint8_t neighbourNumber = neighbors.size();
+        for (uint8_t i = 0; i < neighbourNumber; i++) {
+
+        }
     }
 
 
@@ -40,4 +46,24 @@ uint8_t Map::getHeight() const { return height; }
 
 const map<pair<uint8_t, uint8_t>, vector<pair<uint8_t, uint8_t>>> &Map::getGraph() const {
     return graph;
+}
+
+const set<pair<uint8_t, uint8_t>> &Map::getWinningPath() const {
+    return winningPath;
+}
+
+vector<pair<uint8_t, uint8_t>> &Map::getNeighbours(pair<uint8_t, uint8_t> &vertex) const {
+    vector<pair<uint8_t, uint8_t>> neighbours = {make_pair(vertex.first - 1, vertex.second),
+                                                 make_pair(vertex.first + 1, vertex.second),
+                                                 make_pair(vertex.first, vertex.second - 1),
+                                                 make_pair(vertex.first, vertex.second + 1)};
+    return neighbours;
+}
+
+bool Map::isValidCoordinate(pair<uint8_t, uint8_t> &vertex, uint8_t width, uint8_t height) const {
+    return !(vertex.first < 0 || vertex.first >= width || vertex.second < 0 || vertex.second >= height);
+}
+
+bool Map::isVisited(pair<uint8_t, uint8_t> &vertex, set<pair<uint8_t, uint8_t>> &path) const {
+    return path.count(vertex) > 0;
 }
--- src/Map/Map.h
+++ src/Map/Map.h
@@ -5,6 +5,7 @@
 #include<utility>
 #include<map>
 #include<vector>
+#include<set>
 
 using namespace std;
 
@@ -12,19 +13,28 @@ class Map {
     uint8_t width;
     uint8_t height;
     map<pair<uint8_t, uint8_t>, vector<pair<uint8_t, uint8_t>>> graph;
-    vector<pair<uint8_t, uint8_t>> winningPath;
+    set<pair<uint8_t, uint8_t>> winningPath;
 
 public:
     Map(uint8_t w, uint8_t h);
 
     void draw();
 
+    const map<pair<uint8_t, uint8_t>, vector<pair<uint8_t, uint8_t>>> &getGraph() const;
+    const set<pair<uint8_t, uint8_t>> &getWinningPath() const;
+    vector<pair<uint8_t, uint8_t>> &getNeighbours(pair<uint8_t, uint8_t>& vertex) const;
+
+    bool isValidCoordinate(pair<uint8_t, uint8_t> &vertex, uint8_t width, uint8_t height) const;
+
+    bool isVisited(pair<uint8_t, uint8_t>& vertex, set<pair<uint8_t, uint8_t>>& path) const;
+
 private:
     uint8_t getWidth() const;
 
     uint8_t getHeight() const;
 
-    const map<pair<uint8_t, uint8_t>, vector<pair<uint8_t, uint8_t>>>& getGraph() const;
+    void filterByCoordinate(vector<pair<uint8_t, uint8_t>>& neighbors);
+    void filterByVisit(vector<pair<uint8_t, uint8_t>>& neighbors);
 };
 
 #endif
