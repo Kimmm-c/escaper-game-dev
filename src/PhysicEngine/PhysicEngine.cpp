6c83315f70272caff2b3bc12e10fb121467e5782
Kimmm-c <97310663+Kimmm-c@users.noreply.github.com>
2024-09-19 08:25:53 -0700

    fixed detect collision logic

--- src/PhysicEngine/PhysicEngine.cpp
+++ src/PhysicEngine/PhysicEngine.cpp
@@ -1,13 +1,20 @@
 #include "PhysicEngine.h"
 
+#include <iostream>
+
 bool PhysicEngine::detectCollision(const pair<uint8_t, uint8_t> &currentPosition, int direction, const Map *map) const {
+    /*
+     * Return true (collide) if current position is not connected to next position.
+     * Otherwise, return false (not collide).
+     */
+
     pair<uint8_t, uint8_t> nextPosition = map->getNextPosition(currentPosition, direction);
     auto it = map->getGraph().find(currentPosition);
 
     // If currentPosition exists, check if currentPosition is connected to the next visited position
     if (it != map->getGraph().end()) {
-        return it->second.count(nextPosition) > 0;
+        return it->second.count(nextPosition) <= 0;
     }
 
-    return false;
+    return true;
 }
